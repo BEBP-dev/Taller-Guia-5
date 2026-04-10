dcc703a50081a0f2ad8c3cbc46a1c6f6f1c66602
Brayan Bonilla <802bonillapinerosbrayanesteban@gmail.com>
2026-04-09 19:21:15 -0500

    doc: add docstrings to main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from views.estudiante_view import estudiante_view
 
 def main():
+    """Metodo principal para ejecutar el programa
+    """    
     estudiante_view.main_menu()
 
 if __name__ == "__main__":
