220caa42cd4e4e1f75489f70a6e4cbe588f3fabe
JulitoDev <julianpaez180@gmail.com>
2026-04-11 13:55:05 -0500

    refactor: Class database modified to add a constructor. This will create the data base automaticly

--- database/conexion.py
+++ database/conexion.py
@@ -1,14 +1,17 @@
 import sqlite3
 class database:
-    def conectar():
-        return sqlite3.connect("estudiantes.db")
-    def crear_tabla(conn):
-        conn.execute(
-        """CREATE TABLE IF NOT EXISTS estudiantes(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nombre TEXT,
-        correo TEXT,
-        nota REAL
+    def __init__(self):
+        self.connection = sqlite3.connect("estudiantes.db")
+        self.cursor = self.connection.cursor()
+        self.crear_tabla()
+    
+    def crear_tabla(self):
+        self.cursos.execute(
+            """CREATE TABLE IF NOT EXISTS estudiantes(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre TEXT,
+                correo TEXT,
+                nota REAL
+                )"""
         )
-        """)
-        conn.commit()
+        self.connection.commit()
