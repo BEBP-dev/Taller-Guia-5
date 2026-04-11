d2afa1bcee3ccf11737a6ae419ff3e8ec4c3a78c
JulitoDev <julianpaez180@gmail.com>
2026-04-11 13:26:40 -0500

    fix: Main modified to create the data base directly.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from views.estudiante_view import estudiante_view
-
+from controllers.estudiante_controller import estudiante_controller
+from database.conexion import database
 def main():
     """Metodo principal para ejecutar el programa
     """    
-    estudiante_view.main_menu()
+    vista = estudiante_view()
+    baseDatos = database()
+    controlador = estudiante_controller(vista,baseDatos)
+    
+    controlador.main_menu_controller()
 
 if __name__ == "__main__":
     main()
